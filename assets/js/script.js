9507691fbf9b3dfcc82b0fa3eb6349da9be04a90
Zandile Masilo <zandimasilo@gmail.com>
2024-10-16 09:24:30 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,34 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type")
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        }
+        })
 
     }
+    runGame("addition");
 })
 
 
 
 
+/**
+ * The main game loop called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-function runGame() {
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: $ {gameType}`);
+        throw `unknown game type: $ {gameType}. Aborting!`;
 
+        
+    }
 }
 
 function checkAnaswer(params) {
@@ -42,7 +57,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+
 
 }
 
